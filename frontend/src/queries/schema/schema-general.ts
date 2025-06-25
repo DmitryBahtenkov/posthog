80bc2a5f144b75ea402ae97ac3758d22bf57a92b
Michael Matloka <michael@matloka.com>
2025-06-25 02:06:08 +0200

    WIP

--- frontend/src/queries/schema/schema-general.ts
+++ frontend/src/queries/schema/schema-general.ts
@@ -1151,7 +1151,9 @@ export type FunnelsFilterLegacy = Omit<
 >
 
 export interface FunnelExclusionSteps {
+    /** From which step to apply the particular exclusion. Steps 0-indexed here. */
     funnelFromStep: integer
+    /** Until which step to apply the particular exclusion. Steps 0-indexed here. */
     funnelToStep: integer
 }
 export interface FunnelExclusionEventsNode extends EventsNode, FunnelExclusionSteps {}
